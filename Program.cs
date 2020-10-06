5e1af11fc9c24c11ec836ab386779e80944c599a
Akash <akash.singh670@gmail.com>
2020-10-06 20:30:32 +0530

    [Akash] .Added PIN Code Program

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PINCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the PIN Code : ");
+            String pin = Console.ReadLine();
+            RegexValidation rv = new RegexValidation();
+            rv.Validation(pin);
+        }
+    }
+}
